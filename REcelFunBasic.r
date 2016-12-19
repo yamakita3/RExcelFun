5453b51dfa7c08c4a2d5efe0bc27cbaac7e61082
yamakita <yamakita3@gmail.com>
2016-12-19 20:58:28 +0900

    Update REcelFunBasic.r

--- REcelFunBasic.r
+++ REcelFunBasic.r
@@ -1,3 +1,6 @@
 
-#Example; a <- 1:3; countif.excel(a,"==2");countif.excel(a,">1")
+#Example; 
+#a <- 1:3; countif.excel(a,"==2");countif.excel(a,">1")
+#equal to ;
+#sum(a==2); sum(a>1)
 countif.excel <-function(x,text){ eval(parse(text =paste("sum(x",text,")",sep="")) ) }
